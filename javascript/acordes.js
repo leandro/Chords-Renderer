d72a174eee5a855bbcb6c68e868f278229201886
leandro <leandro@4fc6d720-cb44-0410-88bf-b103adb906d8>
2008-02-03 17:05:52 +0000

    - drag.js adicionado - drag and drop funcionando - variações funcionando
    A FAZER:
    - funcionar pestanas em cordas do meio no opera
    - enxugar e modularizar mais o código, principalmente no bloco do jQuery.
    
    git-svn-id: http://websaudavel.com/svn-acordes@6 4fc6d720-cb44-0410-88bf-b103adb906d8

--- javascript/acordes.js
+++ javascript/acordes.js
@@ -1,63 +1,83 @@
 $(function() {
-	var teste = 0;
 	$('.js-acorde').each(function() {
-		var _0_s, _0_acordes = (_0_s = $.trim($(this).html())).split(/\s+/),
-			_0_min = _0_acordes[min(_0_acordes)],
-			_0_max = _0_acordes[max(_0_acordes)],
-			_0_graphics = cordas(_0_acordes),
-			_0_map = pestana(_0_acordes),
-			_0_pestanas = object_remove_keys(_0_map.map, _0_map.pestanas),
+		var _0_s,
 			_0_i = 0,
 			_0_t,
 			_0_o,
-			_0_dx = 12,
-			_0_dy = 7,
 			_0_diff,
 			_0_dw,
 			_0_tmp,
 			_0_dedos = [],
 			_0_dedo_atual = 1,
+			_0_pos = 0,
+			_0_htmlobj = $(this).get(0),
+			_0_fn;
+		
+		(_0_fn = function(obj) {
+			var _0_acordes = (_0_s = $.trim($(obj).find('div:eq(1)').html())).split(/\s+/),
+			_0_min = _0_acordes[min(_0_acordes)],
+			_0_max = _0_acordes[max(_0_acordes)],
+			_0_graphics = cordas(_0_acordes),
+			_0_dx = 12,
+			_0_dy = 21,
+			_0_map = pestana(_0_acordes),
+			_0_pestanas = object_remove_keys(_0_map.map, _0_map.pestanas),
 			_0_inc = _0_max > 5 ? 0 : _0_min - 1;
 
-		$(this).attr('title', _0_s);
-		$(this).html('');
-		
-		// desenhando os marcadores de como as cordas devem ser tocadas
-		$(this).append(['<div class="acorde-bull acorde-bull-p', _0_graphics.disc,'"><img src="imagens/dec.disc.gif" width="5" height="5"></div>'].join(''));
-		for(_0_o = _0_graphics.xis, _0_t = _0_o.length; _0_i < _0_t; _0_i++)
-			$(this).append(['<div class="acorde-bull acorde-bull-p', _0_o[_0_i],'"><img src="imagens/dec.xis.gif" width="5" height="5"></div>'].join(''));
-		for(_0_o = _0_graphics.circles, _0_i = 0, _0_t = _0_o.length; _0_i < _0_t; _0_i++)
-			$(this).append(['<div class="acorde-bull acorde-bull-p', _0_o[_0_i],'"><img src="imagens/dec.circle.gif" width="5" height="5"></div>'].join(''));
-		// desenhando as pestanas
-		for(_0_i = 0, _0_o = _0_map.pestanas, _0_t = _0_o.length; _0_i < _0_t; _0_i++) {
-			var sub = merge_all(object_values(_0_map.map, function(o, e) { return e < _0_o[_0_i]; }), true),
-				conflito = 6 - _0_pestanas[_0_o[_0_i]][0] - array_diff(_0_pestanas[_0_o[_0_i]], sub).length;
-			//alert(to_s(_0_pestanas));
-			_0_dyf = _0_dy + (_0_o[_0_i] - _0_min + _0_inc) * 12;
-			_0_dxf = (_0_tmp = _0_pestanas[_0_o[_0_i]][0]) * 11 + _0_dx;
-			_0_dw = 60 - _0_tmp * 11 - (sub.length && conflito * 11 || 0);
-			if(_0_max > 5)
-				$(this).append(['<div class="acorde-pricasa">', _0_min,'</div>'].join(''));
-			$(this).append(['<div class="acorde-pestana" style="left:', _0_dxf,'px;top:', _0_dyf,'px;width:', _0_dw,'px;"></div>'].join(''));
-		}
-		// desenhando as posicoes dos dedos
-		var cordas_arr = [];
-		object_remove_keys(_0_map.map, ['0', 'X']);
-		for(_0_i = 0, _0_tmp = [], _0_o = object_keys(_0_map.map).sort(), _0_t = _0_o.length; _0_i < _0_t; _0_i++) {
-			_0_tmp = _0_tmp.concat(repeat_to_array(_0_o[_0_i], _0_map.map[_0_o[_0_i]].length));
-			cordas_arr = cordas_arr.concat(_0_map.map[_0_o[_0_i]]);
-		}
-		_0_dy = 4;
-		_0_dx = 10;
-		for(_0_i = 0, _0_o = _0_tmp, _0_tmp = object_keys(_0_pestanas), _0_t = _0_o.length; _0_i < _0_t; _0_i++, _0_dedo_atual = 1) {
-			_0_dedo_atual += lesser_than(_0_tmp, _0_o[_0_i]).length + _0_i;
-			_0_dyf = _0_dy + (_0_o[_0_i] - _0_min + _0_inc) * 12;
-			_0_dxf = cordas_arr[_0_i] * 11 + _0_dx;
-			$(this).append(['<div class="acorde-dedo" style="left:', _0_dxf,'px;top:', _0_dyf,
-				'px;"><img src="imagens/dec.dedo', _0_dedo_atual,'.gif" width+"9" height="9" /></div>'].join(''));
-		}
+			$(obj).attr('title', _0_s);
+			$(obj).children('*').not('h4').hide();
+			!_0_pos && $(obj).append('<h4 class="acorde-head"><div>' + $(obj).find('div:eq(0)').html() + '<a href="#">&gt;</a></div></h4>');
+			!_0_pos && $(obj).find('a').bind('click', function() {
+				var t = $(_0_htmlobj).find('li').length, pos = (++_0_pos) % t;
+				if(!t) return false;
+				$(_0_htmlobj).find('div:eq(1)').html($(_0_htmlobj).find('li:eq(' + pos + ')').html());
+				_0_fn(_0_htmlobj);
+			});
+			!_0_pos && Drag.init($(obj).find('h4').get(0), $(obj).get(0));
+			$('h4', obj).nextAll().remove();
+
+			// desenhando os marcadores de como as cordas devem ser tocadas
+			$(obj).append(['<div class="acorde-bull acorde-bull-p', _0_graphics.disc,'"><img src="imagens/dec.disc.gif" width="5" height="5"></div>'].join(''));
+			for(_0_i = 0, _0_o = _0_graphics.xis, _0_t = _0_o.length; _0_i < _0_t; _0_i++)
+				$(obj).append(['<div class="acorde-bull acorde-bull-p', _0_o[_0_i],'"><img src="imagens/dec.xis.gif" width="5" height="5"></div>'].join(''));
+			for(_0_o = _0_graphics.circles, _0_i = 0, _0_t = _0_o.length; _0_i < _0_t; _0_i++)
+				$(obj).append(['<div class="acorde-bull acorde-bull-p', _0_o[_0_i],'"><img src="imagens/dec.circle.gif" width="5" height="5"></div>'].join(''));
+			// desenhando as pestanas
+			for(_0_i = 0, _0_o = _0_map.pestanas, _0_t = _0_o.length; _0_i < _0_t; _0_i++) {
+				var sub = merge_all(object_values(_0_map.map, function(o, e) { return e < _0_o[_0_i]; }), true),
+					conflito = 6 - _0_pestanas[_0_o[_0_i]][0] - array_diff(_0_pestanas[_0_o[_0_i]], sub).length;
+				_0_dyf = _0_dy + (_0_o[_0_i] - _0_min + _0_inc) * 12;
+				_0_dxf = (_0_tmp = _0_pestanas[_0_o[_0_i]][0]) * 11 + _0_dx;
+				_0_dw = 60 - _0_tmp * 11 - (sub.length && conflito * 11 || 0);
+				if(_0_max > 5)
+					$(obj).append(['<div class="acorde-pricasa">', _0_min,'</div>'].join(''));
+				$(obj).append(['<div class="acorde-pestana" style="left:', _0_dxf,'px;top:', _0_dyf,'px;width:', _0_dw,'px;"></div>'].join(''));
+			}
+			// desenhando as posicoes dos dedos
+			var cordas_arr = [];
+			object_remove_keys(_0_map.map, ['0', 'X']);
+			for(_0_i = 0, _0_tmp = [], _0_o = object_keys(_0_map.map).sort(), _0_t = _0_o.length; _0_i < _0_t; _0_i++) {
+				_0_tmp = _0_tmp.concat(repeat_to_array(_0_o[_0_i], _0_map.map[_0_o[_0_i]].length));
+				cordas_arr = cordas_arr.concat(_0_map.map[_0_o[_0_i]]);
+			}
+			_0_dy = 18;
+			_0_dx = 10;
+			for(_0_i = 0, _0_o = _0_tmp, _0_tmp = object_keys(_0_pestanas), _0_t = _0_o.length; _0_i < _0_t; _0_i++, _0_dedo_atual = 1) {
+				_0_dedo_atual += lesser_than(_0_tmp, _0_o[_0_i]).length + _0_i;
+				_0_dyf = _0_dy + (_0_o[_0_i] - _0_min + _0_inc) * 12;
+				_0_dxf = cordas_arr[_0_i] * 11 + _0_dx;
+				$(obj).append(['<div class="acorde-dedo" style="left:', _0_dxf,'px;top:', _0_dyf,
+					'px;"><img src="imagens/dec.dedo', _0_dedo_atual,'.gif" width+"9" height="9" /></div>'].join(''));
+			}
+		})(this);
 	});
 });
+
+function variacoes(htmlobj) {
+	var r = [], els = $(htmlobj).find('div:eq(1)').add($(htmlobj).find('li')).get(), t = els.length;
+	if(t > 1) $(els).each(function() { r[r.length] = this.innerHTML; });
+	return r;
+}
 
 function pressed_fingers(acorde_arr) {
 	// conta quantos numeros acima de 0 diferentes entre si existem no acorde para saber se será necessário pestana
@@ -132,6 +152,15 @@ function repeat_to_array(value, times) {
 	return r;
 }
 
+function array_rot(arr, num, dir) {
+	var t = arr.length, r;
+	if(t == num) return arr;
+	dir = dir ? (typeof dir == 'string' ? (dir.toLowerCase() == 'left' ? -1 : 1) : (dir < 0 ? -1 : 1) ) : 1; // -dir -> 'left'; dir -> 'right'
+	if(dir == -1) r = [].concat(arr.slice(num % t), arr.slice(0, num % t));
+	else r = [].concat(arr.slice(t - (num % t)), arr.slice(0, t - (num % t)));
+	return r;
+}
+
 function lesser_than(arr, num) {
 	// retorna todos os numeros menores que num
 	var i = 0, t = arr.length, r = [];
@@ -179,19 +208,19 @@ function array_diff(a, b) {
 function is_enum(obj) {
 	return obj instanceof Array ? true : (typeof obj == 'object' ? true : false);
 }
-function merge_all(enum, recursive) {
-	var type = enum instanceof Array ? 'array' : (typeof enum == 'object' ? 'object' : false), r = [];
+function merge_all(obj, recursive) {
+	var type = obj instanceof Array ? 'array' : (typeof obj == 'object' ? 'object' : false), r = [];
 	if(!type) return r;
 	
 	if(type == 'array') {
-		var i = 0, t = enum.length;
+		var i = 0, t = obj.length;
 		for(; i < t; i++) {
-			r = r.concat(is_enum(enum[i]) && recursive && merge_all(enum[i], true) || enum[i]);
+			r = r.concat(is_enum(obj[i]) && recursive && merge_all(obj[i], true) || obj[i]);
 		}
 	} else {
-		for(var e in enum) {
+		for(var e in obj) {
 			if(!(e in Object.prototype)) {
-				r = r.concat(is_enum(enum[e]) && recursive && merge_all(enum[e], true) || enum[e]);
+				r = r.concat(is_enum(obj[e]) && recursive && merge_all(obj[e], true) || obj[e]);
 			}
 		}
 	}
